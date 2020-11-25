38db0b0abf147dd2e5055da526977d69cdc582ac
Nick Pavlov <gurinderu@gmail.com>
2020-11-25 21:49:20 +0300

    wip

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "proto/data.pb.h"
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+  functork.api.apf::Apf m;
+  cout << "Hello Proto!" << endl;
+  return 0;
+}
