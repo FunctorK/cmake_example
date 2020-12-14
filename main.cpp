6e253336705fcdb8df5ea3837d92216b8ad23f66
Nick Pavlov <gurinderu@gmail.com>
2020-12-14 20:45:51 +0300

    Fix ApfId parsing

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
-#include "proto/data.pb.h"
+#include <memory>
+#include <string>
 
-using namespace std;
-int main(int argc, char const *argv[])
-{
-  functork.api.apf::Apf m;
-  cout << "Hello Proto!" << endl;
-  return 0;
+#include <grpc++/grpc++.h>
+#include <google/protobuf/empty.pb.h>
+#include "functorK/api/apf/service.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using google::protobuf::Empty;
+using functorK::api::apf::ApfStructure;
+using functorK::api::apf::ApfApi;
+
+int main(int argc, char **argv) {
+    auto channel = grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
+    auto stub = functorK::api::apf::ApfApi::NewStub(channel);
+
+    ClientContext context;
+
+    Empty request;
+    ApfStructure response;
+
+    Status status = stub->FetchApfStructure(&context, request, &response);
+    std::cout << "Reply received: " << response.organization() << std::endl;
+
+    return 0;
 }
